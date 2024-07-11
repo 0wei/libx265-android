d7c487d9004bfe349bdad17726dee49b342bb731
luowei <luv135@163.com>
2024-07-11 13:38:37 +0800

    build android success

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 
+rm -rf x265/build/android 
+rm -rf build
 mkdir x265/build/android
 cp -rf build_script/* x265/build/android
 
 
 #export NDK_ROOT="path/to/ndk-build" # you must export your ndk root. eg. usr/username/android-ndk-r16b. you can download ndk from this url:https://developer.android.com/ndk/downloads/
+export NDK_ROOT=/home/wei/android-ndk-r17c
+
 export ANDROID_API_VERSION=26 #chose android platform version. I recommend a version higher than 21.
 export NUMBER_OF_CORES=4
 export OUTPUT_PREFIX=$(pwd)/build
@@ -12,27 +16,27 @@ pushd x265/build/android/arm64-v8a
 bash build.sh
 popd
 
-pushd x265/build/android/armeabi
-bash build.sh
-popd
+# pushd x265/build/android/armeabi
+# bash build.sh
+# popd
 
 pushd x265/build/android/armeabi-v7a
 bash build.sh
 popd
 
-pushd x265/build/android/mips
-bash build.sh
-popd
+# pushd x265/build/android/mips
+# bash build.sh
+# popd
 
-pushd x265/build/android/mips64
-bash build.sh
-popd
+# pushd x265/build/android/mips64
+# bash build.sh
+# popd
 
-pushd x265/build/android/x86
-bash build.sh
-popd
+# pushd x265/build/android/x86
+# bash build.sh
+# popd
 
-pushd x265/build/android/x86_64
-bash build.sh
-popd
+# pushd x265/build/android/x86_64
+# bash build.sh
+# popd
 
